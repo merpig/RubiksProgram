394cd5db6b4f7c2210cd2a7aab28cf55fa39969e
Sasha Peters <peteram1@sewanee.edu>
2019-11-15 14:38:24 -0600

    info panel touch ups

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -26,30 +26,33 @@ const Navbar = props => (
     <div style={{float:"right"}} >
     <Popup trigger={<button id="triggerBtn">Info</button>}>
         {close => (
-          <div className="popupDiv">
-            <b style={{fontSize: "2rem"}}>Instructions</b>
-            <button className="close" id="closeBtn" onClick={close}>
-              &times;
-            </button>
-            <hr style={{backgroundColor:"#007bff",marginBottom:"0"}}></hr>
-            <div style={{backgroundColor:"lightgrey",marginTop:"0",color:"black"}}>
-              <p style={{paddingTop: "1rem"}}>Use arrow keys to turn the cube.</p>
-              <hr style={{backgroundColor:"lightgray"}}></hr>
-              <p>Use buttons on the right or keyboard: 
-                <b style={{color: "white",backgroundColor:"black"}}>F,</b><b style={{color: "white",backgroundColor:"black"}}>f,</b>
-                <b style={{color: "blue",backgroundColor:"black"}}>U,</b><b style={{color: "blue",backgroundColor:"black"}}>u,</b>
-                <b style={{color: "red",backgroundColor:"black"}}>R,</b><b style={{color: "red",backgroundColor:"black"}}>r,</b>
-                <b style={{color: "yellow",backgroundColor:"black"}}>B,</b><b style={{color: "yellow",backgroundColor:"black"}}>b,</b>
-                <b style={{color: "orange",backgroundColor:"black"}}>L,</b><b style={{color: "orange",backgroundColor:"black"}}>l,</b>
-                <b style={{color: "green",backgroundColor:"black"}}>D,</b><b style={{color: "green",backgroundColor:"black"}}>d</b> 
-                to turn individual faces
-              </p>
-              <hr style={{backgroundColor:"lightgray"}}></hr>
-              <p>Top Left buttons control the speed</p>
-              <hr style={{backgroundColor:"lightgray"}}></hr>
-              <p>Bottom left buttons are built in pattern functions</p>
-              <hr style={{backgroundColor:"lightgray"}}></hr>
-              <p style={{marginBottom: "0",paddingBottom:"1rem"}}>Bottom right buttons are the core functions for the cube</p>
+          <div>
+            <div className="shadeBackground" onClick={close}></div>
+            <div className="popupDiv">
+              <b style={{fontSize: "2rem"}}>Instructions</b>
+              <button className="close" id="closeBtn" onClick={close}>
+                &times;
+              </button>
+              <hr style={{backgroundColor:"#007bff",marginBottom:"0"}}></hr>
+              <div style={{backgroundColor:"lightgrey",marginTop:"0",color:"black"}}>
+                <p style={{paddingTop: "1rem"}}>Use arrow keys to turn the cube.</p>
+                <hr style={{backgroundColor:"lightgray"}}></hr>
+                <p>Use buttons on the right or keyboard: 
+                  <b style={{color: "white",backgroundColor:"black"}}> F, </b><b style={{color: "white",backgroundColor:"black"}}>f,</b>
+                  <b style={{color: "blue",backgroundColor:"black"}}> U, </b><b style={{color: "blue",backgroundColor:"black"}}>u,</b>
+                  <b style={{color: "red",backgroundColor:"black"}}> R, </b><b style={{color: "red",backgroundColor:"black"}}>r,</b>
+                  <b style={{color: "yellow",backgroundColor:"black"}}> B, </b><b style={{color: "yellow",backgroundColor:"black"}}>b,</b>
+                  <b style={{color: "orange",backgroundColor:"black"}}> L, </b><b style={{color: "orange",backgroundColor:"black"}}>l,</b>
+                  <b style={{color: "green",backgroundColor:"black"}}> D, </b><b style={{color: "green",backgroundColor:"black"}}>d </b> 
+                  to turn individual faces
+                </p>
+                <hr style={{backgroundColor:"lightgray"}}></hr>
+                <p>Top Left buttons control the speed</p>
+                <hr style={{backgroundColor:"lightgray"}}></hr>
+                <p>Bottom left buttons are built in pattern functions</p>
+                <hr style={{backgroundColor:"lightgray"}}></hr>
+                <p style={{marginBottom: "0",paddingBottom:"1rem"}}>Bottom right buttons are the core functions for the cube</p>
+              </div>
             </div>
           </div>
         )}
