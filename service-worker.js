2381f4a5374ea2406591382f7a734b5c92ec9019
Sasha Peters <peteram1@sewanee.edu>
2020-04-24 13:46:55 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RubiksProgram/precache-manifest.be429b8ec1d887112de231665bdf10be.js"
+  "/RubiksProgram/precache-manifest.a4be1d805e93d5773a417538a39ee653.js"
 );
 
 self.addEventListener('message', (event) => {
