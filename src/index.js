8d881aa884727ea4661e682937c0acdf57cdea9f
Sasha Peters <peteram1@sewanee.edu>
2020-07-10 11:56:43 -0500

    fixed paths

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+      <div>
+        <Route path="*" component={App} />
+      </div>
+    </Router>
+  )
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
