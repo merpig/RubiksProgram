c9958a86309838d7275e65125f8c47abdeffcd73
Sasha Peters <peteram1@sewanee.edu>
2020-09-24 20:09:33 -0500

    Updates

---
+++ precache-manifest.3f79dd00535842aa29ba7a90f8a5496a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1733df4e2642d6bea7c3f5a8ba437918",
+    "url": "/RubiksProgram/index.html"
+  },
+  {
+    "revision": "5ab01668381b4a69e29a",
+    "url": "/RubiksProgram/static/css/2.b6c0fe55.chunk.css"
+  },
+  {
+    "revision": "6bddfa38d3ba3453cc10",
+    "url": "/RubiksProgram/static/css/main.ead43c0b.chunk.css"
+  },
+  {
+    "revision": "5ab01668381b4a69e29a",
+    "url": "/RubiksProgram/static/js/2.fe70d9e5.chunk.js"
+  },
+  {
+    "revision": "24d5231598efc6a7d0882dfb53a46aaf",
+    "url": "/RubiksProgram/static/js/2.fe70d9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bddfa38d3ba3453cc10",
+    "url": "/RubiksProgram/static/js/main.2b0b1ab1.chunk.js"
+  },
+  {
+    "revision": "c0b09aae083631d78863",
+    "url": "/RubiksProgram/static/js/runtime-main.8ddb94a0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RubiksProgram/precache-manifest.780d8bf3e72de1f2936058473ed47fb5.js"
+  "/RubiksProgram/precache-manifest.3f79dd00535842aa29ba7a90f8a5496a.js"
 );
 
 self.addEventListener('message', (event) => {
