ed8da8d186626b570689bff7fc832756e07095c5
Sasha Peters <peteram1@sewanee.edu>
2020-09-27 10:51:55 -0500

    info panel has been updated to better match the rest of the site

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -67,17 +67,16 @@ const Navbar = props => {
         {close => (
           <div style={{zIndex:"100",width:"100%",height:"100%"}}>
             <div className="shadeBackground" style={{backgroundColor:"black",zIndex:"101"}} onClick={close}></div>
-            <div style={{zIndex:"102",width:"max-content",margin:"auto",transform:"translateX(-50%)"}} className="popupDiv">
+            <div style={{zIndex:"102",width:"max-content",margin:"auto",transform:"translateX(-50%)",padding:"8px"}} className="popupDiv">
               <b style={{fontSize: "2rem"}}>Instructions</b>
               <button className="close" id="closeBtn" onClick={close}>
                 &times;
               </button>
-              <hr style={{backgroundColor:"#007bff",marginBottom:"0"}}></hr>
-              <div style={{backgroundColor:"black",marginTop:"0",color:"#00C000",fontSize:"1rem",textAlign:"left"}}>
-                <div style={{paddingTop: "1rem"}}>&gt; Click and drag anywhere not on the cube to rotate.</div>
-                <hr style={{backgroundColor:"lightgray"}}></hr>
-                <div>&gt; 1. Click and drag anywhere on the cube to make a move or</div>
-                <div>&gt; 2. Use buttons(Moves) or keyboard to make turns: <br></br>
+              <div style={{backgroundColor:"rgba(0,0,0,.8)",marginTop:"0",color:"lightgrey",fontSize:"1rem",textAlign:"center",borderRadius:".25rem",padding:"8px"}}>
+                <div style={{paddingTop: "1rem"}}>Click and drag anywhere not on the cube to rotate.</div>
+                <hr style={{backgroundColor:"lightgray",width:"60%"}}></hr>
+                <div>Click and drag anywhere on the cube to make a move <br></br>or use the keyboard to make turns:</div>
+                <div>
                   <div style={{width:"100%",textAlign:"center"}}>
                   <b style={{color: "white",backgroundColor:"black"}}> F, </b><b style={{color: "white",backgroundColor:"black"}}>f,</b>
                   <b style={{color: "blue",backgroundColor:"black"}}> U, </b><b style={{color: "blue",backgroundColor:"black"}}>u,</b>
@@ -88,8 +87,8 @@ const Navbar = props => {
                   <p style={{width:"100%",textAlign:"center"}}>(lower case is clockwise, upper case is counterclockwise)</p>
                   </div>
                 </div>
-                <hr style={{backgroundColor:"lightgray"}}></hr>
-                <div style={{marginBottom: "0",paddingBottom:"1rem"}}>&gt; Author: Sasha Peters, <a target="#" href="https://www.github.com/merpig">github</a>, <a target="#" href="https://www.github.com/merpig/RubiksProgram">Project Repo</a></div>
+                <hr style={{backgroundColor:"lightgray",width:"60%"}}></hr>
+                <div style={{marginBottom: "0",paddingBottom:"1rem"}}>Author: Sasha Peters <br></br> <a target="#" href="https://www.github.com/merpig"><i className="fa fa-github" style={{fontSize:"36px",marginRight:"10px"}}></i></a><a target="#" href="https://www.linkedin.com/in/alexandr-sasha-peters-8a2489168/"><i className="fa fa-linkedin" style={{fontSize:"36px"}}></i></a></div>
                 <hr style={{backgroundColor:"lightgrey"}}></hr>
                   <div style={{color: "white",backgroundColor:"black",textAlign:"center"}}>
                   site design / cube © 2020 RubiksProgram
