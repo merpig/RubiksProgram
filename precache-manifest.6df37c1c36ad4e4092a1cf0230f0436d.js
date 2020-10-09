6a5d69e6e1932b87916cde5a54002c318afdd245
Sasha Peters <peteram1@sewanee.edu>
2020-10-09 07:39:21 -0500

    Updates

--- precache-manifest.6df37c1c36ad4e4092a1cf0230f0436d.js
+++ precache-manifest.6df37c1c36ad4e4092a1cf0230f0436d.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "89c7e56d83de1fb5277f74755dd18622",
+    "revision": "143e17b7d201a98e8892a271f0234e46",
     "url": "/RubiksProgram/index.html"
   },
   {
-    "revision": "e44989160654613626ac",
+    "revision": "d622bfa256b6208c9fd9",
     "url": "/RubiksProgram/static/css/2.b6c0fe55.chunk.css"
   },
   {
-    "revision": "7a6b0ab55964fd216256",
+    "revision": "53e8ee06deba012fe78d",
     "url": "/RubiksProgram/static/css/main.547026f8.chunk.css"
   },
   {
-    "revision": "e44989160654613626ac",
-    "url": "/RubiksProgram/static/js/2.ca035f7d.chunk.js"
+    "revision": "d622bfa256b6208c9fd9",
+    "url": "/RubiksProgram/static/js/2.b74d0230.chunk.js"
   },
   {
     "revision": "24d5231598efc6a7d0882dfb53a46aaf",
-    "url": "/RubiksProgram/static/js/2.ca035f7d.chunk.js.LICENSE.txt"
+    "url": "/RubiksProgram/static/js/2.b74d0230.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7a6b0ab55964fd216256",
-    "url": "/RubiksProgram/static/js/main.68d7cece.chunk.js"
+    "revision": "53e8ee06deba012fe78d",
+    "url": "/RubiksProgram/static/js/main.7c8e7a5c.chunk.js"
   },
   {
     "revision": "c0b09aae083631d78863",
