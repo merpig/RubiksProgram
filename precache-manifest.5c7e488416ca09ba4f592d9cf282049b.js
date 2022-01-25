8998aaa8536c78d3b2ff2876706826eddd1b2e95
peteram1 <peteram1@sewanee.edu>
2022-01-25 11:26:58 -0600

    Updates

---
+++ precache-manifest.5c7e488416ca09ba4f592d9cf282049b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaaab826d9203682da67e434ee0bf94d",
+    "url": "/RubiksProgram/index.html"
+  },
+  {
+    "revision": "b9e04b124a5d4b28fe7b",
+    "url": "/RubiksProgram/static/css/2.cce07776.chunk.css"
+  },
+  {
+    "revision": "f48b64cf433d3a6de68c",
+    "url": "/RubiksProgram/static/css/main.8f7d9a36.chunk.css"
+  },
+  {
+    "revision": "b9e04b124a5d4b28fe7b",
+    "url": "/RubiksProgram/static/js/2.3af2107b.chunk.js"
+  },
+  {
+    "revision": "4377836474120fbe5b40671095655181",
+    "url": "/RubiksProgram/static/js/2.3af2107b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f48b64cf433d3a6de68c",
+    "url": "/RubiksProgram/static/js/main.04c57591.chunk.js"
+  },
+  {
+    "revision": "c0b09aae083631d78863",
+    "url": "/RubiksProgram/static/js/runtime-main.8ddb94a0.js"
+  }
+]);
